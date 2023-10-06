5aba44c0cc0512d14f8e55d4dd500927570ee587
eunikehp <104567399+eunikehp@users.noreply.github.com>
2023-10-06 16:17:05 +0200

    only author can edit the post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
 	ListView,
 	DetailView,
@@ -26,6 +26,7 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
 	model = Post
 
+
 class PostCreateView(CreateView):
 	model = Post
 	fields = ['title','content']
@@ -35,13 +36,19 @@ class PostCreateView(CreateView):
 		return super().form_valid(form)
 
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	model = Post
 	fields = ['title','content']
 
 	def form_valid(self, form):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
 	
 
 def about(request):
