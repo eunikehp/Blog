1466ee322631baaae20843c007c5866e3861e60f
eunikehp <104567399+eunikehp@users.noreply.github.com>
2023-10-11 11:49:41 +0200

    email and password reset

--- django_project/settings.py
+++ django_project/settings.py
@@ -130,3 +130,15 @@ LOGIN_URL = 'login'
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 # media url is where we access that image in the browser
 MEDIA_URL = '/media/'
+
+import os
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+
+
